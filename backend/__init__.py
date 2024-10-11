b075ee0077288b84dabd2154d8531a2835ac7c06
matsusht24 <matsusht24@up.edu>
2024-10-11 13:55:23 -0700

    Getting bg card data via api

--- backend/__init__.py
+++ backend/__init__.py
@@ -64,8 +64,42 @@ def comp_parser():
     file.close()
 
    
-        
-    
+def get_minions():
+    # Your Blizzard API credentials
+    client_id = os.getenv('HS_CLIENT_ID')
+    client_secret = os.getenv('HS_CLIENT_SECRET')
+    print(client_id)
+
+    # Request OAuth token
+    url = 'https://oauth.battle.net/token'
+    data = {'grant_type': 'client_credentials'}
+
+    response = requests.post(url, data=data, auth=(client_id, client_secret))
+
+    # Extract the access token from the response
+    if response.status_code == 200:
+        access_token = response.json()['access_token']
+        print(f"Access Token: {access_token}")
+    else:
+        print("Failed to retrieve access token")
+        return
+    url = 'https://us.api.blizzard.com/hearthstone/cards'
+    params = {
+        'locale': 'en_US',     # Language
+        'gameMode': 'battlegrounds',
+        'class': 'neutral',    # Neutral minions
+        'type': 'minion',      # Card type: minion
+        'access_token': access_token,  # Auth token
+    }
+    response = requests.get(url, params=params)
+
+    # Check the response and print the data
+    if response.status_code == 200:
+        minions = response.json()
+        for minion in minions['cards']:
+            print(f"Name: {minion['name']}, Type: {minion['type']}")
+    else:
+        print(f"Failed to retrieve minions: {response.status_code}")
 
 
 def hero_parser():
@@ -77,6 +111,6 @@ def hero_parser():
 def main():
     #hero_parser()
     #comp_parser()
-    print("for testing")
+    get_minions()
 
 main()
