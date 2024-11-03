0e97c9b894fe5de1f36c40356676349336ee479d
matsusht24 <matsusht24@up.edu>
2024-11-02 23:28:16 -0700

    started creating fetch for hero info

--- client/hsbg_helper/pages/hero/page.tsx
+++ client/hsbg_helper/pages/hero/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import HeroCard from '../ui/hero_card'
 
 
 function Page() {
+  const [heroData, setHeroData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchHeroData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/heroes'); 
+        if (!response.ok) throw new Error("Failed to fetch data");
+        const data = await response.json();
+        setHeroData(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchHeroData();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+  if (!heroData.length) return <p>Loading...</p>;
+
+
   return (
         <main className='m-10'>
           <header className='text-center font-bold text-4xl p-12'>Hero Selection</header>
